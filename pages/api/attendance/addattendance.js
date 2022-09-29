885f2d4a3c8c31e5f0426f032d88ef5ca9d4bfa2
Winston D. Mequila <winstonmequila@gmail.com>
2022-09-30 07:46:41 +0800

    Update addattendance.js

--- pages/api/attendance/addattendance.js
+++ pages/api/attendance/addattendance.js
@@ -3,52 +3,21 @@ import { dbConnect } from "../../../connect/dbconnect";
 import Attendance from "../../../model/Attendance";
 import { errorHandler,responseHandler } from "../../../util/common";
 
-import multer from "multer";
-import nc from "next-connect";
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const path = require('path')
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(process.cwd(), "public", "uploads"));
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().getTime() + "-" + file.originalname);
-    },
-  }),
-});
 
 
-const handler = nc({
-  onError: (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).end("Something broke!");
-  },
-  onNoMatch: (req, res) => {
-    res.status(404).end("Page is not found");
-  },
-})
-  .use(upload.single('image'))
-  .get((req, res) => {
-    res.send("Hello world eee");
-  })
-  .post( async (req, res) => {
+export default async function handler(req, res) {
+
+  if (req.method !== "POST") {
+
+    errorHandler("Invalide Request Type is not a POST", res);
+
+  } else {      
+
 
     try {
 
-      //const session = await getSession({req});
-      // if(session){
-      //   errorHandler("Access denied", res);
-      // }else {
-
-        const {empId,totalmin,timeIn,timeOut,day,month,year,lat,lng,datetime,locId,userId} = req.body;
+      
+        const {empId,totalmin,timeIn,timeOut,day,month,year,lat,lng,datetime,image,locId,userId} = req.body;
          
   
 
@@ -66,7 +35,7 @@ const handler = nc({
         lng:lng,
         datetime:datetime,
         locId:locId,
-        image:"/uploads/" + req.file.filename,
+        image:image,
         userId:userId
        
       });
@@ -76,8 +45,6 @@ const handler = nc({
 
       if (saveAtt) {
        
-        // responseHandler(saveAtt, res, 200);
-        // dbConnect.close();
 
         res.status(200).json(saveAtt)
         
@@ -91,14 +58,7 @@ const handler = nc({
       errorHandler(error, res);
     }
 
-  })
-  .put(async (req, res) => {
-    res.end("async/await is also supported!");
-  })
-  .patch(async (req, res) => {
-    throw new Error("Throws me around! Error can be caught and handled.");
-  });
+  }
+}
 
-
-  export default handler;
   
