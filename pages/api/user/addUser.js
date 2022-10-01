7d240d898b043ef28fdc63274c6a6fb3c25d14d2
Winston D. Mequila <winstonmequila@gmail.com>
2022-10-01 09:18:30 +0800

    Update addUser.js

--- pages/api/user/addUser.js
+++ pages/api/user/addUser.js
@@ -1,104 +1,58 @@
 import { dbConnect } from "../../../connect/dbconnect";
 import { errorHandler,responseHandler } from "../../../util/common";
 import User from "../../../model/User";
-
-
-
-
 import bcrypt from "bcrypt";
-import multer from "multer";
-import nc from "next-connect";
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const path = require('path')
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.join(process.cwd(), "public", "users"));
-    },
-    filename: function (req, file, cb) {
-      cb(null, new Date().getTime() + "-" + file.originalname);
-    },
-  }),
-});
 
 
-const handler = nc({
-  onError: (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).end("Something broke!");
-  },
-  onNoMatch: (req, res) => {
-    res.status(404).end("Page is not found");
-  },
-})
-  .use(upload.single('image'))
-  .get((req, res) => {
-    res.send("Hello world eee");
-  })
-  .post( async (req, res) => {
+export default async function handler(req, res) {
 
-    try {
+    if (req.method !== "POST") {
+  
+      errorHandler("Invalide Request Type is not a POST", res);
+  
+    } else {      
 
-      //const session = await getSession({req});
-      // if(session){
-      //   errorHandler("Access denied", res);
-      // }else {
+  
+      try {
 
-        const { email,password} = req.body;
-         
-        console.log('ddd',email,password);
-        
-      await dbConnect();
+      const { email,password,image} = req.body;
+
+      console.log('paylod',email,password,image)
 
       const hashPassword = await bcrypt.hash(password, 8);
 
+      await dbConnect();   
+
+
       const user = new User({
-        ...req.body,
+        email: email,
         password: hashPassword,
-        image: "/users/" + req.file.filename,
+        image:image,
         userlevel: 0
       });
 
       const saveUser = await user.save();
-
+  
       if (saveUser) {
+
         const userDoc = saveUser._doc;
         delete userDoc.password;
 
-        //responseHandler(saveUser, res, 200);
-         res.status(200).json(({ body: "OK" }))
-
+        responseHandler(saveUser, res, 200);
         
-        
-        dbConnect.close();
         
       } else {
         errorHandler("Something went wrong", res);
       }
+        
+  
 
 
-
-
-     // }
-
-    }catch(error){
-      errorHandler(error, res);
+      } catch (error) {
+        errorHandler(error, res);
+      }
     }
+  }
 
-  })
-  .put(async (req, res) => {
-    res.end("async/await is also supported!");
-  })
-  .patch(async (req, res) => {
-    throw new Error("Throws me around! Error can be caught and handled.");
-  });
 
-export default handler;
 
