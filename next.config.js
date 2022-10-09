889810e1507f11a69d3d3097341a20ab89ebd203
winstonmeq <winstonmequila@gmail.com>
2022-10-09 20:02:55 +0800

    unpdate env2

--- next.config.js
+++ next.config.js
@@ -9,10 +9,11 @@ const nextConfig = {
   env: {
     //dre eh butang ang domain.. dire lng mag change automatic na ma update ang mga domain
 
-    NEXTAUTH_SECRET: 'v7COYqKpEdnCbd5aISAw9BxjupOLKYCgBVZ2kwusMNs=',
-
-     NEXTAUTH_URL: 'attendance2-flame.vercel.app',
+   
+     NEXTAUTH_URL: 'https://attendance2-flame.vercel.app',
    // NEXTAUTH_URL: 'http://192.168.102.18:3000',
+     NEXTAUTH_SECRET: 'v7COYqKpEdnCbd5aISAw9BxjupOLKYCgBVZ2kwusMNs=',
+
  },
  
    async headers() {
